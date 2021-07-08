40b352e40b82de3e9d0e9253f87bad5f12cd1f95
Malika939 <84612422+Malika939@users.noreply.github.com>
2021-07-08 11:48:34 +0600

    dgs1

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,21 +1,38 @@
 from django.db import models
-
+from django.db.models.fields import DateTimeField
 
 class News(models.Model):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
-    content = models.TextField('Текст', blank=True)
-    create_at = models.DateTimeField('Дата создания', auto_now_add=True)
-    update_at = models.DateTimeField('Дата обновления', auto_now=True)
-    photo = models.ImageField('Фото', upload_to='photos/%Y/%m/%d')
-    is_published = models.BooleanField('Опубликовано', default=True)
+    content = models.TextField(blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    is_published = models.BooleanField(default=True)
+    category = models.ForeignKey('Category',
+                                 on_delete=models.CASCADE,
+                                 null=True,
+                                 default=1,
+                                 verbose_name='Наименование катугории')
 
-
-    def __str__(self):
+    def str(self):
         return f'{self.title}'
 
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-create_at']
+
+class Category(models.Model):
+    title = models.CharField(max_length=155,
+                             db_index=True,
+                             verbose_name='Наименование категории')
+    def __str__(self):
+        return  f'{self.title}'
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
 
 
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
 from .views import *
 
 
 urlpatterns = [
     path('', main),
+    path('category/<int:category_id>', get_category),
 ]
 
 
